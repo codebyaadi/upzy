368641b2e67fb50abd84c77a4d7bc32fc3198ae1
Aditya Rajbhar <adityastar57@gmail.com>
2025-05-22 14:39:02 +0000

    package: added prettier-config package

---
+++ packages/prettier-config/base.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "es5",
+};
+
+export default config;
