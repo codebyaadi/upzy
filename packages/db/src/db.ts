2e80994ab3210076a116d81e23fde13a80fbd410
Aditya Rajbhar <adityastar57@gmail.com>
2025-06-19 17:29:02 +0000

    db: made the package to work for nestjs

---
+++ packages/db/src/db.ts
@@ -0,0 +1,16 @@
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
+
+export { schema };
+export * from "./schema";
+
+export function createDatabase(connectionString: string) {
+  const pool = new Pool({
+    connectionString,
+  });
+
+  return drizzle(pool, { schema });
+}
+
+export type Database = ReturnType<typeof createDatabase>;
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,2 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-
-export const db = drizzle(process.env.DATABASE_URL!);
+export * from "./db";
+export * from "./schema";
