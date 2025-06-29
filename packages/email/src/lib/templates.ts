9cf807a075e8f6fb2626ad836f0d666fdf45a166
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2025-06-29 14:34:42 +0000

    feat(email): added new `MagicLinkEmail` template

--- packages/email/src/lib/templates.ts
+++ packages/email/src/lib/templates.ts
@@ -1,10 +1,12 @@
 import PasswordResetEmail from "../templates/PasswordResetEmail";
 import WelcomeEmail from "../templates/WelcomeEmail";
+import MagicLinkEmail from "../templates/MagicLinkEmail";
 import { EmailTemplates } from "../types";
 
 // Map template names to their components
 export const TEMPLATES: EmailTemplates = {
   WelcomeEmail,
   PasswordResetEmail,
+  MagicLinkEmail,
   // Add all your other email templates here
 };
--- packages/email/src/types/index.ts
+++ packages/email/src/types/index.ts
@@ -1,10 +1,12 @@
 import PasswordResetEmail from "../templates/PasswordResetEmail";
 import WelcomeEmail from "../templates/WelcomeEmail";
+import MagicLinkEmail from "../templates/MagicLinkEmail";
 
 // Define a type for mapping template names to their React components and props
 export type EmailTemplates = {
   WelcomeEmail: typeof WelcomeEmail;
   PasswordResetEmail: typeof PasswordResetEmail;
+  MagicLinkEmail: typeof MagicLinkEmail;
   // Add all your other email templates here
   // Example: 'OrderConfirmation': typeof OrderConfirmationEmail;
 };
