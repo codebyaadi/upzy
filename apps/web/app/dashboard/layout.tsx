1f046a8dea7d629bba9b42fa85bd86299f0fbaf4
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2025-07-06 14:57:35 +0000

    feat(web): updated dashboard for sidebar

--- apps/web/app/dashboard/layout.tsx
+++ apps/web/app/dashboard/layout.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { AppSidebar } from "@/components/layouts/sidebar/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@upzy/ui/components/sidebar";
+import { Separator } from "@upzy/ui/components/separator";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="relative">{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </div>
+        </header>
+        <main>{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
