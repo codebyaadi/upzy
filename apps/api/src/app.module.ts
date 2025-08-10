06532ef662c7a8d0f78a8b4a5db7f2ce68521511
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2025-08-10 17:37:16 +0000

    feat(api): added scheduler module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -8,6 +9,7 @@ import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { envSchema } from './config/env.schema';
 import { MonitorModule } from './monitor/monitor.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
   imports: [
@@ -21,11 +23,13 @@ import { MonitorModule } from './monitor/monitor.module';
         return parsed.data;
       },
     }),
+    ScheduleModule.forRoot(),
     DatabaseModule,
     TestsModule,
     AuthModule,
     HealthModule,
     MonitorModule,
+    SchedulerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
