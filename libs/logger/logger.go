7348b014821e3556cc4f86cc28a8f0b01623ce09
Aditya Rajbhar <100792725+codebyaadi@users.noreply.github.com>
2025-08-12 17:46:43 +0000

    feat(libs:logger): updated logger for golang

--- libs/logger/logger.go
+++ libs/logger/logger.go
@@ -1,44 +1,104 @@
 package logger
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log/slog"
 	"os"
+
+	"github.com/fatih/color"
 )
 
-var Logger *slog.Logger
+type Logger struct {
+	*slog.Logger
+}
 
-func Init(level string) {
-	var logLevel slog.Level
-	switch level {
-	case "debug":
-		logLevel = slog.LevelDebug
-	case "info":
-		logLevel = slog.LevelInfo
-	case "warn":
-		logLevel = slog.LevelWarn
-	case "error":
-		logLevel = slog.LevelError
-	default:
-		logLevel = slog.LevelInfo
+const (
+	contextKey = "context"
+)
+
+type PrettyHandler struct {
+	slog.Handler
+	writer      io.Writer
+	serviceName string
+	pid         int
+}
+
+type HandlerOptions struct {
+	SlogOpts    slog.HandlerOptions
+	ServiceName string
+}
+
+func New(serviceName string) *Logger {
+	opts := HandlerOptions{
+		ServiceName: serviceName,
+		SlogOpts: slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
 	}
 
-	Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: logLevel,
-	}))
+	color.NoColor = false
+	handler := NewPrettyHandler(os.Stdout, opts)
+	slogLogger := slog.New(handler)
+	return &Logger{slogLogger}
 }
 
-func Info(msg string, args ...any) {
-	Logger.Info(msg, args...)
+func (l *Logger) Fatal(msg string, args ...any) {
+	l.Error(msg, args...)
+	os.Exit(1)
 }
 
-func Error(msg string, args ...any) {
-	Logger.Error(msg, args...)
+func NewPrettyHandler(out io.Writer, opts HandlerOptions) *PrettyHandler {
+	return &PrettyHandler{
+		writer:      out,
+		serviceName: opts.ServiceName,
+		pid:         os.Getpid(),
+		Handler:     slog.NewTextHandler(out, &opts.SlogOpts),
+	}
 }
 
-func Debug(msg string, args ...any) {
-	Logger.Debug(msg, args...)
-}
+func (h *PrettyHandler) Handle(_ context.Context, r slog.Record) error {
 
-func Warn(msg string, args ...any) {
-	Logger.Warn(msg, args...)
+	green := color.New(color.FgGreen).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintFunc()
+
+	levelStr := r.Level.String() + ":"
+	switch r.Level {
+	case slog.LevelDebug:
+		levelStr = color.MagentaString(levelStr)
+	case slog.LevelInfo:
+		levelStr = color.GreenString(levelStr)
+	case slog.LevelWarn:
+		levelStr = color.YellowString(levelStr)
+	case slog.LevelError:
+		levelStr = color.RedString(levelStr)
+	}
+
+	var logContext string
+	r.Attrs(func(attr slog.Attr) bool {
+		if attr.Key == contextKey {
+			logContext = attr.Value.String()
+			return false
+		}
+		return true
+	})
+	if logContext == "" {
+		logContext = "Application"
+	}
+
+	timestamp := r.Time.Format("01/02/2006, 03:04:05 PM")
+
+	logString := fmt.Sprintf("%s %s - %s   %-18s %s %s\n",
+		green(fmt.Sprintf("[%s]", h.serviceName)),
+		yellow(h.pid),
+		timestamp,
+		levelStr,
+		cyan(fmt.Sprintf("[%s]", logContext)),
+		r.Message,
+	)
+
+	_, err := h.writer.Write([]byte(logString))
+	return err
 }
